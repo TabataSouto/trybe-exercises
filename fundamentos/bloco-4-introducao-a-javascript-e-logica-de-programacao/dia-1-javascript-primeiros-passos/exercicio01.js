1f9cece55046be1eb8c7a8c33778061a9d360874
Tabata <tabatac.souto@gmail.com>
2021-11-06 13:23:59 -0300

    Pratica: exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio01.js
@@ -1,3 +1,5 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados.
+
 const a = 5;
 const b = 12;
 
