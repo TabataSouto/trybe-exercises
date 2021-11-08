7692cb157645aa56ddb880013a06b6a87a9efb40
Tabata <tabatac.souto@gmail.com>
2021-11-08 12:21:47 -0300

    Pratica: exercicio 8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio08.js
@@ -0,0 +1,10 @@
+//Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+const num1 = 12; //par
+const num2 = 27; //impar
+const num3 = 5; //impar
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
+    console.log("true")
+} else {
+    console.log("false")
+}
