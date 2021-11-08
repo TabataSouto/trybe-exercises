c539f37de64f01ea581fdb73f9bbc411f7c4a7d5
Tabata <tabatac.souto@gmail.com>
2021-11-08 18:43:07 -0300

    Correçao exercicio 01 e 02

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array_e_loop_for/exercicio02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array_e_loop_for/exercicio02.js
@@ -1,3 +1,5 @@
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0; //esse resultado tem que estar zerado, não em branco
 
