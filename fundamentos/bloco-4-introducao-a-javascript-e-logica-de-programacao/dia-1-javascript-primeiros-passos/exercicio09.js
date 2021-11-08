cb6b694454a9af4ec6977b94d145f78480bff5fc
Tabata <tabatac.souto@gmail.com>
2021-11-08 15:20:01 -0300

    Exercicio sobre aprendizado do Array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio09.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio09.js
@@ -1,10 +1,10 @@
 //Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 
 const num1 = 12; //par
-const num2 = 2; //impar
-const num3 = 4; //impar
+const num2 = 27; //impar
+const num3 = 5; //impar
 
-if (num1 % 2 == 1 || num2 % 2 == 1 || num3 % 2 == 1){
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
     console.log("true")
 } else {
     console.log("false")
