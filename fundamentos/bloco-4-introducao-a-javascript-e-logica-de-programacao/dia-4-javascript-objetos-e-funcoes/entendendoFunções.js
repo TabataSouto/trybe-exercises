0ee5136bff7c01b97c0ac5fee992ac2fadcc64bd
Tabata <tabatac.souto@gmail.com>
2021-11-10 20:54:55 -0300

    Para fixas + exercicios de 1 a 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/entendendoFunções.js
@@ -0,0 +1,27 @@
+//Funções de um carro-----------------
+//------------------------------------
+
+//Ligar/Desligar
+
+let statusCarro = "desligado";
+
+function ligarDesligar () {
+    if (statusCarro === "desligado") {
+        statusCarro = "ligado"; 
+    } else {
+        statusCarro = "desligado";
+    }
+    return statusCarro;
+}
+
+//Acelerar
+
+//Frear
+
+//Direcionar
+
+//Ligar/Desligar os faróis
+
+//Dar seta
+
+//Dar ré
