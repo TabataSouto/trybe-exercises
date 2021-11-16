d7ae54c98aa0cb1303936985fb150660ef38f095
Tabata <tabatac.souto@gmail.com>
2021-11-16 19:17:06 -0300

    Exercicios seletores e elementos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/getElementBy/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/getElementBy/script.js
@@ -6,10 +6,16 @@ document.getElementById('page-title').innerText = 'Final Fantasy VII: Advent Chi
 
 // 2 Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 const paragraph = document.getElementById('second-paragraph');
-paragraph.style.padding = '15px';
+paragraph.style.padding = '10px';
 paragraph.style.display = 'inline';
 paragraph.style.backgroundColor = 'blue';
 paragraph.style.color = 'white';
 
 // 3. Por fim, recupere o subtítulo e altere-o também.
 document.getElementById('subtitle').innerText = 'Muito legal!';
+
+//4 -Adicione uma classe igual para os dois parágrafos e recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName: altere algum estilo do primeiro deles
+let books = document.getElementsByClassName('meus-livros')[0].style.color = 'green';
+
+//. Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName .
+document.getElementsByTagName('h4')[1].style.color = 'gray';
