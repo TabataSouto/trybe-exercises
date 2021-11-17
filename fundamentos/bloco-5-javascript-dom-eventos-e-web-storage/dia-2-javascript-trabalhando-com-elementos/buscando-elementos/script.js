bc5604abbc82460afd4fe697ef93ecb5ac74ebaa
Tabata <tabatac.souto@gmail.com>
2021-11-17 18:48:51 -0300

    Alteraçao para fixar: buscando elementos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/buscando-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/buscando-elementos/script.js
@@ -1,15 +1,15 @@
 // Acesse o elemento elementoOndeVoceEsta .
 //console.log(document.querySelector('#elementoOndeVoceEsta'))
-document.querySelector('#elementoOndeVoceEsta');
+document.getElementById('#elementoOndeVoceEsta');
 
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 // parentNode e parentElement são propriedades que retornam elemento/nó pai
 //console.log(document.querySelector('#elementoOndeVoceEsta').parentElement.style.backgroundColor = '#7180AC')
-document.querySelector('#elementoOndeVoceEsta').parentElement.style.backgroundColor = '#7180AC';
+document.getElementById('elementoOndeVoceEsta').parentElement.style.color = '#7180AC';
 
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 // console.log(document.getElementById('primeiroFilhoDoFilho').innerText = 'Alterando o texto!')
-document.getElementById('primeiroFilhoDoFilho').innerText = 'Alterando o texto!'
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Colocando um novo texto!'
 
 // Acesse o primeiroFilho a partir de pai .
 // console.log(document.getElementById('pai').firstElementChild)
@@ -26,8 +26,7 @@ document.getElementById('elementoOndeVoceEsta').previousElementSibling;
 document.getElementById('elementoOndeVoceEsta').nextSibling;
 
 // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
-console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
-
+//console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+document.getElementById('elementoOndeVoceEsta').nextElementSibling
 
 // Agora acesse o terceiroFilho a partir de pai .
-// console.log(document.getElementById('pai').);
