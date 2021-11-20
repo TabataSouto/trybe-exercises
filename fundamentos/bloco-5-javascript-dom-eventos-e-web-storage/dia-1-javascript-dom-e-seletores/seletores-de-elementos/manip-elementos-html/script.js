7e55ac2ab3530cfb8e52e4ced087011e63d90a8a
Tabata <tabatac.souto@gmail.com>
2021-11-20 13:04:22 -0300

    Para Fixar: querySelector e getElement finalizado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/manip-elementos-html/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/manip-elementos-html/script.js
@@ -1,20 +1,27 @@
-document.querySelectorAll('#header-container')[0].style.backgroundColor = '#036b52';
+// 1. colocar o background color no rgb da trybe em id header-container;
+const bgHeader = document.getElementById('header-container');
+bgHeader.style.backgroundColor = '#33CA7F';
 
-let sections = document.getElementsByTagName('section');
-sections[0].style.backgroundColor = 'rgb(255, 147, 120)';
-sections[1].style.backgroundColor = 'rgb(240,200,90)';
+// 2. na classe emergency-tasks colocar bg salmão, e na classe no-emergency-tasks na esquerda e bg amarelo na direita;
+const bgEmergency = document.querySelector('.emergency-tasks');
+bgEmergency.style.backgroundColor = '#FBD1A2';
 
-let subTitles = document.querySelectorAll('.emergency-tasks h3');
-subTitles[0].style.backgroundColor = 'rgb(153, 50, 204)';
-subTitles[1].style.backgroundColor = 'rgb(153, 50, 204)';
+const bgNoEmergency = document.getElementsByClassName('no-emergency-tasks');
+bgNoEmergency[0].style.backgroundColor = '#ECE4B7';
 
-let subTitlesTwo = document.getElementsByTagName('h3');
-subTitlesTwo[2].style.backgroundColor = 'rgb(0, 0, 0)';
-subTitlesTwo[3].style.backgroundColor = 'black';
+// 3. colocar bg purple no h3 da esquerda e black na direita;
+const bg1H3 = document.querySelectorAll('.emergency-tasks div h3');
+for (let i = 0; i < bg1H3.length; i += 1) {
+  bg1H3[i].style.backgroundColor = '#A491D3';
+}
 
-// let footerBg = document.getElementById('footer-container');
-// footerBg[0].style.backgroundColor = '#6B8E23'
+const bg2H3 = document.querySelectorAll('.no-emergency-tasks div h3');
+for (let i = 0; i < bg2H3.length; i += 1) {
+  bg2H3[i].style.backgroundColor = '#222223';
+}
 
-let footerBg = document.getElementsByClassName('footer-container');
-footerBg[0].style.backgroundColor = '#006400';
-footerBg[0].style.padding = '10px';
+// 4. colocar bg verde musgo no class footer-container
+const footer = document.getElementsByTagName('footer');
+footer[0].style.backgroundColor = '#1C3738';
+footer[0].style.color = 'white';
+footer[0].style.padding = '10px';
