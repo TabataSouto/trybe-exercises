c48ada8a544859b73ff10069d72c59756ced3c5d
Tabata <tabatac.souto@gmail.com>
2021-11-20 12:02:46 -0300

    Para Fixar getElements Tag e ClassName: finalizado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/getElementBy/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/seletores-de-elementos/getElementBy/script.js
@@ -1,21 +1,16 @@
-// const paragraph = document.getElementById("paragraph");
-//       paragraph.style.color = "red";
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red"
 
 // 1. Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
-document.getElementById('page-title').innerText = 'Final Fantasy VII: Advent Children';
+const title = document.getElementById('page-title');
+title.innerText = 'Final Fantasy VII: Advent Children';
 
-// 2 Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
-const paragraph = document.getElementById('second-paragraph');
-paragraph.style.padding = '10px';
-paragraph.style.display = 'inline';
-paragraph.style.backgroundColor = 'blue';
-paragraph.style.color = 'white';
+// 2. Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+const secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.style.display = 'inline';
+secondParagraph.style.backgroundColor = '#E3B9BC';
+secondParagraph.style.padding = '15px';
 
 // 3. Por fim, recupere o subtítulo e altere-o também.
-document.getElementById('subtitle').innerText = 'Muito legal!';
-
-//4 -Adicione uma classe igual para os dois parágrafos e recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName: altere algum estilo do primeiro deles
-let books = document.getElementsByClassName('meus-livros')[0].style.color = 'green';
-
-//. Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName .
-document.getElementsByTagName('h4')[1].style.color = 'gray';
+const subTitle = document.getElementById('sub-title');
+subTitle.innerText = 'Apenas para teste!'
