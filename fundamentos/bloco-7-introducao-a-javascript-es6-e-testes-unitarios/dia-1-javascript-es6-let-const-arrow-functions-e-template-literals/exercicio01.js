b920e54b4b57b3ee07d16e4894b3f1a50483821b
Tabata <tabatac.souto@gmail.com>
2021-12-03 18:42:50 -0300

    Exercicio um finalizado

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio01.js
@@ -0,0 +1,31 @@
+// 1. Modifique a estrutura da função para que ela seja uma arrow function .
+// 2. Modifique as concatenações para template literals .
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+
+// função modificada;
+let teste = (escopo) => {
+  if (escopo === true) {
+    var ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    var elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+    console.log(elseScope);
+  }
+  console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+teste(true)
