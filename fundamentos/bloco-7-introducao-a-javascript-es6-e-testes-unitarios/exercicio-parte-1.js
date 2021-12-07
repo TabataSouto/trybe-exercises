effcc3d0462f4679b2374e2b24b49953e8ca3ada
Tabata <tabatac.souto@gmail.com>
2021-12-07 20:27:01 -0300

    exercicio parte 1 finalizado

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/exercicio-parte-1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/exercicio-parte-1.js
@@ -16,3 +16,14 @@ const scope = (escopo) => {
 }
 
 scope(true);
+
+
+// -----------------------------------------------------------------------------------------
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+// Seu código aqui.
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
