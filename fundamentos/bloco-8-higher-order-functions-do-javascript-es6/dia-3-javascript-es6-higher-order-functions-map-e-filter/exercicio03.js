eaa983dac33a46370070e0e897503c5c09750599
Tabata <tabatac.souto@gmail.com>
2022-01-03 17:39:39 -0300

    Exercicio 03: corrigindo operador ou

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio03.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio03.js
@@ -62,6 +62,6 @@ const books = [
 ];
 
 // 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
-const fantasyOrScienceFiction = books.filter(book => book.genre === 'Fantasia' | book.genre === 'Ficção Científica');
+const fantasyOrScienceFiction = books.filter(book => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
 
 console.log(fantasyOrScienceFiction);
