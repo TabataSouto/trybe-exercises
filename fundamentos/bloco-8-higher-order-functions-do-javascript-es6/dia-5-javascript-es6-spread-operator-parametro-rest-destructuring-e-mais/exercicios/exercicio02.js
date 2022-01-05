82017e03099bc8654f8a8fb8b888b0a37f470263
Tabata <tabatac.souto@gmail.com>
2022-01-05 19:11:43 -0300

    Exercicio 02: criar funçao de soma com quantidade de elementos variaveis no parametro

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios/exercicio02.js
@@ -0,0 +1,6 @@
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+function sum (...value) {
+  return value.reduce((acc, val) => acc + val, 0);
+};
+
+console.log(sum(10, 10));
